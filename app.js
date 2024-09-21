720c8d94a2568842b1fafddc27a08f5ff4fabd02
Raphael Lima <rapha.lima.rl16@gmail.com>
2024-09-20 21:53:42 -0300

    Subindo servidor e criando a primeira rota

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path');
+
+const routerPrincipal = require('./routes/principal');
+
+const app = express();
+const porta = 3000;
+
+// configurando ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//configurando arquivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routers
+app.use('/login', routerPrincipal);
+
+app.listen(porta, () => {
+    console.log('Servidor ouvindo na porta ' + porta);
+});
